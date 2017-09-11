df7c89996c09a076da98b30dcd03f42bd9c4f879
Kozma István <pisti_kozma@hotmail.com>
2017-09-11 13:39:23 +0200

    feat(w02-d01): finish average_of_input

--- week-02/day-01/average_of_input.py
+++ week-02/day-01/average_of_input.py
@@ -2,3 +2,17 @@
 # then it should print the sum and the average of these numbers like:
 #
 # Sum: 22, Average: 4.4
+
+user_input = []
+input_sum = 0
+avg = 0
+
+for i in range(5):
+    number = int(input("enter an integer: "))
+    user_input.append(number)
+
+input_sum = sum(user_input)
+avg = input_sum / len(user_input)
+
+print(input_sum)
+print(avg)
