09d945c19f96b574154bc24d1c0fc8d193dca736
Kozma István <pisti_kozma@hotmail.com>
2017-09-11 13:49:56 +0200

    fix(w02-d01): coding_hours: fix average hours calculation

--- week-02/day-01/coding_hours.py
+++ week-02/day-01/coding_hours.py
@@ -11,11 +11,10 @@ daily_coding_hours = 6
 hours_coded_per_week = 5 * daily_coding_hours
 semester_length_in_weeks = 17
 
-avg_working_hours = 52
-total_hours_in_week = 24 * 7
-hours_coded_in_semester =  avg_working_hours * semester_length_in_weeks
-total_hours_in_semester = total_hours_in_week * semester_length_in_weeks
+avg_working_hours_in_week = 52
+hours_coded_in_semester =  avg_working_hours_in_week * semester_length_in_weeks
+total_workhours_in_semester = avg_working_hours_in_week * semester_length_in_weeks
 
 
 print("hours spent coding in semester: " + str(hours_coded_per_week * semester_length_in_weeks))
-print("average hours spent coding per week: " + str(hours_coded_in_semester / total_hours_in_semester))
+print("average hours spent coding per week: " + str(hours_coded_per_week * 100 // avg_working_hours_in_week))
