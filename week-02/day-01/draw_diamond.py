0ee3292dbb152e3a6b831850778f946d167427cd
Kozma István <pisti_kozma@hotmail.com>
2017-09-11 15:00:56 +0200

    feat(w02-d01): finish draw_diamond

--- week-02/day-01/draw_diamond.py
+++ week-02/day-01/draw_diamond.py
@@ -11,3 +11,12 @@
 #    *
 #
 # The diamond should have as many lines as the number was
+
+diamond_height = int(input("Enter an integer: "))
+
+for i in range(diamond_height):
+    print(" " * (diamond_height-i) + "*" * (i*2) + "*")
+
+
+for i in range(diamond_height, -1 , -1):
+    print(" " * (diamond_height-i) + "*" * (i*2) + "*")
