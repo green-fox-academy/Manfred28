dcf6350a45ffb17094a5121142ab2483369c27ba
Kozma István <pisti_kozma@hotmail.com>
2017-09-11 15:04:39 +0200

    feat(w02-d01): finish draw_square

--- week-02/day-01/draw_square.py
+++ week-02/day-01/draw_square.py
@@ -10,3 +10,11 @@
 # %%%%%
 #
 # The square should have as many lines as the number was
+
+square_height = int(input("Enter an integer: "))
+
+for i in range(square_height):
+    if i == 0 or i == square_height - 1:
+        print("%" * square_height)
+    else:
+        print("%" + " " * (square_height - 2) + "%")
