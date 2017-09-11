914f0a1a67c18b7590a55da61c1ce46e0bf11a18
Kozma István <pisti_kozma@hotmail.com>
2017-09-11 12:30:49 +0200

    feat(w02-d01): complete coding_hours

--- week-02/day-01/coding_hours.py
+++ week-02/day-01/coding_hours.py
@@ -7,3 +7,15 @@
 # Print the percentage of the coding hours in the semester if the average
 # work hours weekly is 52
 
+daily_coding_hours = 6
+hours_coded_per_week = 5 * daily_coding_hours
+semester_length_in_weeks = 17
+
+avg_working_hours = 52
+total_hours_in_week = 24 * 7
+hours_coded_in_semester =  avg_working_hours * semester_length_in_weeks
+total_hours_in_semester = total_hours_in_week * semester_length_in_weeks
+
+
+print(hours_coded_per_week * semester_length_in_weeks)
+print(hours_coded_in_semester / total_hours_in_semester)
