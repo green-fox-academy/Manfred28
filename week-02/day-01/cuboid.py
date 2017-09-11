7d927b9028cf85d29ac8369438b3a3226daef307
Kozma István <pisti_kozma@hotmail.com>
2017-09-11 13:21:04 +0200

    feat(w02-d01): complete cuboid

--- week-02/day-01/cuboid.py
+++ week-02/day-01/cuboid.py
@@ -3,3 +3,13 @@
 # 
 # Surface Area: 600
 # Volume: 1000
+
+
+def cuboid_surface_area (width, length, height):
+    return 2 * width * length + 2 * width * height + 2 * length * height
+
+def cuboid_volume(width, length, height):
+    return width * length * height
+
+print("Surface Area: " + str(cuboid_surface_area(100, 200, 300)))
+print("Volume: " + str(cuboid_volume(100, 200, 300)))
