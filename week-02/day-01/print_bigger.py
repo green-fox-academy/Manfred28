a15f731e70f3720bff91b4f654e80c1cd84e3531
Kozma István <pisti_kozma@hotmail.com>
2017-09-11 13:55:47 +0200

    feat(w02-d01): finish print_bigger

--- week-02/day-01/print_bigger.py
+++ week-02/day-01/print_bigger.py
@@ -1,1 +1,9 @@
 # Write a program that asks for two numbers and prints the bigger one
+
+num1 = int(input("Enter a number: "))
+num2 = int(input("Enter a number: "))
+
+if num1 > num2:
+    print(num1)
+else: 
+    print(num2)
