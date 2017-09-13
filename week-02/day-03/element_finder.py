751261931cd016c4debf0defdbd35b5082ef524f
Kozma István <pisti_kozma@hotmail.com>
2017-09-13 12:01:49 +0200

    feat(w02-d03): finish element_finder

---
+++ week-02/day-03/element_finder.py
@@ -0,0 +1,10 @@
+# Check if the list contains "7" if it contains print "Hoorray" otherwise print "Noooooo"
+
+numbers = [1, 2, 3, 4, 5, 6, 8];
+
+magic_number = 7
+
+if magic_number in numbers: 
+    print("Hoorray")
+else:
+    print("Noooooo")
