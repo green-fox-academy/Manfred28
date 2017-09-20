9da81023a1e4e450ac963fe12f842d165f63546c
Kozma István <pisti_kozma@hotmail.com>
2017-09-20 11:11:37 +0200

    feat(w03-d03): implement purple steps 3d with for

--- week-03/day-03/12_purple_steps_3d.py
+++ week-03/day-03/12_purple_steps_3d.py
@@ -9,19 +9,12 @@ def draw_square(x0,y0, width, filler=None):
     canvas.create_rectangle(x0, y0, x0 + width, y0 + width, fill=filler)
 
 
-''' Y U NO WORK
 def draw_steps(growth, square_width, margin, steps):
-    if steps == 0:
-        return
-    offset = steps * square_width + margin
-    square_width = square_width + growth * steps
-    draw_square(offset, offset, square_width, filler="purple")
-    steps -= 1
-    draw_steps(growth, square_width, margin, steps) '''
-        
-
-
-
+    offset = margin
+    for step in range(steps):
+        offset += square_width
+        square_width = square_width + growth
+        draw_square(offset, offset, square_width, filler="purple")
 
 
 draw_steps(growth=10, square_width=5, margin=10, steps=12)
