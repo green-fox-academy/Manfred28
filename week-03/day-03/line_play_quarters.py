11a45b1ff51f3f4f9202615bf2134fb3d221b0e9
Kozma István <pisti_kozma@hotmail.com>
2017-09-20 23:20:21 +0200

    refactor(w03-d03): line_play_quarters - simplify function parameters(?)

--- week-03/day-03/line_play_quarters.py
+++ week-03/day-03/line_play_quarters.py
@@ -9,23 +9,25 @@ canvas.pack()
 # and repeat this pattern in each quarter:
 # [https://github.com/greenfox-academy/teaching-materials/blob/master/workshop/drawing/line-play/r1.png]
 
-def line_play(start, end, step, color, x_offset, y_offset):
-    for i in range(start, end, step):
-        x0 = start + x_offset
+def line_play(step, x_offset, y_offset, is_reverse=False):
+    start_point, end_point = [150, 0] if is_reverse else [0, 150]
+    color = "purple" if is_reverse else "green"
+    for i in range(start_point, end_point, step):
+        x0 = start_point + x_offset
         y0 = i + y_offset
         x1 = i + step + x_offset
-        y1 = end + y_offset
+        y1 = end_point + y_offset
         canvas.create_line(x0, y0, x1, y1, fill=color)
 
-def line_play_quarter(start, end, step, color1, color2, x_offset=0, y_offset=0):
-    line_play(start, end, step, color1, x_offset, y_offset)
-    line_play(end, start, -step, color2, x_offset, y_offset)
+def line_play_quarter(step, x_offset=0, y_offset=0):
+    line_play(step, x_offset, y_offset)
+    line_play(-step, x_offset, y_offset, is_reverse=True)
 
 
-line_play_quarter(0, 150, 10, "green", "purple") # top-left
-line_play_quarter(0, 150, 10, "green", "purple", x_offset=150) # top-right
-line_play_quarter(0, 150, 10, "green", "purple", y_offset=150) # bottom-left
-line_play_quarter(0, 150, 10, "green", "purple", x_offset=150, y_offset=150) # bottom-right
+line_play_quarter(step=10) # top-left
+line_play_quarter(step=10, x_offset=150) # top-right
+line_play_quarter(step=10, y_offset=150) # bottom-left
+line_play_quarter(step=10, x_offset=150, y_offset=150) # bottom-right
 
 
 root.mainloop()
