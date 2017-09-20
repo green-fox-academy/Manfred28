8e32eeccbf96ec79bf467d7693195c2d1844a1e1
Kozma István <pisti_kozma@hotmail.com>
2017-09-20 12:19:51 +0200

    feat(w03-d03): implement line_to_center

--- week-03/day-03/function_to_center.py
+++ week-03/day-03/function_to_center.py
@@ -2,8 +2,10 @@ from tkinter import *
 
 root = Tk()
 
-canvas = Canvas(root, width='300', height='300')
+canvas_size = 300
+canvas = Canvas(root, width=canvas_size, height=canvas_size)
 canvas.pack()
+
 
 # create a line drawing function that takes 2 parameters:
 # the x and y coordinates of the line's starting point
@@ -11,9 +13,16 @@ canvas.pack()
 # fill the canvas with lines from the edges, every 20 px, to the center.
 
 def line_to_center(x, y):
-    canvas.create_line(x,y, 150, 150)
+    canvas.create_line(x, y, canvas_size/2, canvas_size/2)
 
 
-line_to_center(0,0)
+def draw_all_the_lines(step):
+    for i in range(int(canvas_size/step)+1):
+        line_to_center(0, i * step)
+        line_to_center(i * step, 0)
+        line_to_center(canvas_size, i * step)
+        line_to_center(i * step, canvas_size)
 
+
+draw_all_the_lines(20)
 root.mainloop()
