2cf3d59f990b340a4dee7ba42a2e5db336e6d9f5
Kozma István <pisti_kozma@hotmail.com>
2017-09-21 11:03:00 +0200

    feat(w03-d04): implement string1

---
+++ week-03/day-04/string1.py
@@ -0,0 +1,10 @@
+# Given a string, compute recursively (no loops) a new string where all the
+# lowercase 'x' chars have been changed to 'y' chars.
+
+def convert_x_to_y(string):
+    if len(string) == 0:
+        return ""
+    last_char = "y" if string[-1] == "x" else string[-1]
+    return  convert_x_to_y(string[:-1]) + "" + last_char
+
+print(convert_x_to_y("x is y axxxxx"))
