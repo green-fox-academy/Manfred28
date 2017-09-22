8e4f72f1e79757b975e5d213e8ce625ee59bd19c
Kozma István <pisti_kozma@hotmail.com>
2017-09-22 15:29:18 +0200

    refactor(w03-d03): line_play_quarters

--- week-03/day-03/line_play_quarters.py
+++ week-03/day-03/line_play_quarters.py
@@ -9,25 +9,31 @@ canvas.pack()
 # and repeat this pattern in each quarter:
 # [https://github.com/greenfox-academy/teaching-materials/blob/master/workshop/drawing/line-play/r1.png]
 
-def line_play(step, x_offset, y_offset, is_reverse=False):
-    start_point, end_point = [150, 0] if is_reverse else [0, 150]
+def line_play(x_offset=0, y_offset=0, is_reverse=False):
+    origo, canvas_size, step = [150, 0, -10] if is_reverse else [0, 150, 10]
     color = "purple" if is_reverse else "green"
-    for i in range(start_point, end_point, step):
-        x0 = start_point + x_offset
-        y0 = i + y_offset
-        x1 = i + step + x_offset
-        y1 = end_point + y_offset
-        canvas.create_line(x0, y0, x1, y1, fill=color)
+    
+    for i in range(origo, canvas_size, step):
+        canvas.create_line(
+                            x0 = origo + x_offset, 
+                            y0 = i + y_offset,
+                            x1 = i + step + x_offset, 
+                            y1 = canvas_size + y_offset, 
+                            fill=color
+                            )
 
-def line_play_quarter(step, x_offset=0, y_offset=0):
-    line_play(step, x_offset, y_offset)
-    line_play(-step, x_offset, y_offset, is_reverse=True)
 
+line_play() # top-left
+line_play(is_reverse=True) # top-left
 
-line_play_quarter(step=10) # top-left
-line_play_quarter(step=10, x_offset=150) # top-right
-line_play_quarter(step=10, y_offset=150) # bottom-left
-line_play_quarter(step=10, x_offset=150, y_offset=150) # bottom-right
+line_play(x_offset=150) # top-right
+line_play(x_offset=150, is_reverse=True) # top-right
+
+line_play(y_offset=150) # bottom-left
+line_play(y_offset=150, is_reverse=True) # bottom-left
+
+line_play(x_offset=150, y_offset=150) # bottom-right
+line_play(x_offset=150, y_offset=150, is_reverse=True) # bottom-right
 
 
 root.mainloop()
