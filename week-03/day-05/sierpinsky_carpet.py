e7c5a109351538fb4db7cedd19165525e70e89bc
Kozma István <pisti_kozma@hotmail.com>
2017-09-22 13:09:27 +0200

    feat(w03-d05): animate sierpinsky carpet

--- week-03/day-05/sierpinsky_carpet.py
+++ week-03/day-05/sierpinsky_carpet.py
@@ -3,10 +3,54 @@ from random import randint
 
 root = tkinter.Tk()
 
-canvas = tkinter.Canvas(root, width=300, height=300)
+c_width = 1200
+c_height = 800
+
+canvas = tkinter.Canvas(root, width=c_width, height=c_height)
 canvas.pack()
 
 squares = []
+
+
+def move_right():
+    for square in squares:
+        funk_me = randint(1,5)
+        canvas.move(square, +funk_me, 0)
+        canvas.itemconfig(square, fill=random_color())
+
+
+def move_left():
+    for square in squares:
+        funk_me = randint(1,5)    
+        canvas.move(square, -funk_me, 0)
+        canvas.itemconfig(square, fill=random_color())
+
+
+def move_down():
+    for square in squares:
+        funk_me = randint(1,5)    
+        canvas.move(square, 0, +funk_me)
+        canvas.itemconfig(square, fill=random_color())
+
+
+def move_up():
+    for square in squares:
+        funk_me = randint(1,5)    
+        canvas.move(square, 0, -funk_me)
+        canvas.itemconfig(square, fill=random_color())
+
+
+def move_it_move_it():
+    move_left()
+    canvas.after(20, move_down)
+    canvas.after(40, move_right)
+    canvas.after(60, move_up)
+    canvas.after(80, move_it_move_it)
+
+
+def random_color():
+    hexadecimal = "#" + str(randint(0, 9)) + str(randint(0, 9)) + str(randint(0, 9)) + str(randint(0, 9)) + str(randint(0, 9)) + str(randint(0, 9))
+    return hexadecimal
 
 def draw_square(x0,y0, size, filler=None):
     offset = size / 2
@@ -20,7 +64,7 @@ def draw_square(x0,y0, size, filler=None):
 
 
 def sierpinsky(x, y, size):
-    if size < 5:
+    if size < 10:
         return
     squares.append(draw_square(x, y, size, "black"))
     new_size = size / 3
@@ -34,12 +78,14 @@ def sierpinsky(x, y, size):
     sierpinsky(x - size, y, new_size) # left
 
 
-sierpinsky(150, 150, 100)
-
-
-root.mainloop()
 
 
 
 
-# Epilepsy
+def main():
+    sierpinsky(c_width / 2, c_height / 2, 200)
+    move_it_move_it()
+    root.mainloop()
+
+
+main()
