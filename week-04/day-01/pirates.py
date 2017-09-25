26c0ea9b3a1fe1cde224e36075e43ddab1bb64cf
Kozma István <pisti_kozma@hotmail.com>
2017-09-25 17:09:59 +0200

    feat(w04-d01): pirates - add pirate class

---
+++ week-04/day-01/pirates.py
@@ -0,0 +1,39 @@
+from random import randint
+
+class Pirate(object):
+    def __init__(self, parrot):
+        self.drink_counter = 0
+        self.is_conscious = True
+        self.is_alive = True
+        self.is_captain = parrot
+
+    def drink_some_rum(self):
+        self.drink_counter += 1
+
+    def hows_it_going_mate(self):
+        if self.is_alive and self.is_conscious:
+            if self.drink_counter <= 4:
+                print("Pour me anudder!")
+                self.drink_some_rum()
+            else:
+                print("Arghh, I'ma Pirate. How d'ya d'ink its goin?")
+                self.is_conscious = False
+        elif not self.is_alive:
+            print("he's dead")
+        else:
+            print("he's out stupid")
+
+    def die(self):
+        self.is_alive = False
+        self.is_conscious = False
+
+    def brawl(self, enemy):
+        if enemy.is_alive and self.is_alive:
+            roll = randint(1,3)
+            if roll == 1:
+                self.is_alive = False
+            elif roll == 2:
+                enemy.is_alive = False
+            else:
+                self.is_conscious = False
+                enemy.is_conscious = False
