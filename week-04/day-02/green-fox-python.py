ebbe4a41da4f590657481bc9141f6152500dc977
Kozma István <pisti_kozma@hotmail.com>
2017-09-26 12:03:25 +0200

    feat(w04-d02): greenfox - implement Sponsor class

--- week-04/day-02/green-fox-python.py
+++ week-04/day-02/green-fox-python.py
@@ -11,18 +11,20 @@ class Person(object):
     def get_goal(self):
         print("My goal is: Live for the moment!")
 
+
 class Student(Person):
     def __init__(self, name="Jane Doe", age=30, gender="female", previous_organization="The School of Life"):
         super().__init__(name, age, gender)
         self.previous_organization = previous_organization
         self.skipped_days = 0
-    
+   
     def introduce(self):
         params = [self.name, self.age, self.gender, self.previous_organization, self.skipped_days]
-        print("Hi, I'm {}, a {} year old {} from {} who skipped {} days from the course already.".format(*params))        
+        print("Hi, I'm {}, a {} year old {} from {} who skipped {} days from the course already.".format(*params))
 
     def skip_days(self, number_of_days):
         self.skipped_days += number_of_days
+
 
 class Mentor(Person):
     def __init__(self, name="Jane Doe", age=30, gender="female", level="intermediate"):
@@ -35,3 +37,20 @@ class Mentor(Person):
     def introduce(self):
         params = [self.name, self.age, self.gender, self.level]
         print("Hi, I'm {}, a {} year old {} {} mentor.".format(*params))
+
+
+class Sponsor(Person):
+    def __init__(self, name="Jane Doe", age=30, gender="female", company="Google"):
+        super().__init__(name, age, gender)
+        self.company = company
+        self.hired_students = 0
+
+    def introduce(self):
+        params = [self.name, self.age, self.gender, self.company, self.hired_students]
+        print("Hi, I'm {}, a {} year old {} who represents {} and hired {} students so far".format(*params))
+
+    def hire(self):
+        self.hired_students += 1
+
+    def get_goal(self):
+        print("Hire brilliant junior software developers.")
