3c47f44f1c2deb8c9631b3747be95b6f3acecf11
Kozma István <pisti_kozma@hotmail.com>
2017-09-26 11:49:29 +0200

    feat(w04-d02): greenfox - implement Student class

--- week-04/day-02/green-fox-python.py
+++ week-04/day-02/green-fox-python.py
@@ -1,5 +1,5 @@
 class Person(object):
-    def __init__(self, name="Jane Doe", age="30", gender="female"):
+    def __init__(self, name="Jane Doe", age=30, gender="female"):
         self.name = name
         self.age = age
         self.gender = gender
@@ -10,6 +10,19 @@ class Person(object):
     def get_goal(self):
         print("My goal is: Live for the moment!")
 
-jane = Person()
-jane.introduce()
-jane.get_goal()
+class Student(Person):
+    def __init__(self, name="Jane Doe", age=30, gender="female", previous_organization="The School of Life"):
+        super().__init__(name, age, gender)
+        self.previous_organization = previous_organization
+        self.skipped_days = 0
+    
+    def introduce(self):
+        params = [self.name, self.age, self.gender, self.previous_organization, self.skipped_days]
+        print("Hi, I'm {}, a {} year old {} from {} who skipped {} days from the course already.".format(*params))        
+
+    def skip_days(self, number_of_days):
+        self.skipped_days += number_of_days
+
+
+ayy = Student("hallo")
+ayy.introduce()
