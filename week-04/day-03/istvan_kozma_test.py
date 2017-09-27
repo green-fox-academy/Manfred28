53572be953d21aa24f2f638c597a9cb22072c635
Kozma István <pisti_kozma@hotmail.com>
2017-09-27 12:02:41 +0200

    feat(w04-d03): implement sum

--- week-04/day-03/istvan_kozma_test.py
+++ week-04/day-03/istvan_kozma_test.py
@@ -6,7 +6,22 @@ class MyObjectTests(unittest.TestCase):
         self.object = MyObject()
     
     def test_apple_returns_apple_string(self):
-        self.assertEquals(self.object.get_apple(), "appl")
+        self.assertEqual(self.object.get_apple(), "appl")
+
+
+    def test_sum_returns_None_if_empty_list(self):
+        self.assertEqual(self.object.sum_numbers([]), None)
+
+    def test_sum_returns_correct_value_with_one_number(self):
+        self.assertEqual(self.object.sum_numbers([1]), 1)
+
+    def test_sum_returns_correct_value_with_two_numbers(self):
+        self.assertEqual(self.object.sum_numbers([1, 1]), 2)
+        self.assertEqual(self.object.sum_numbers([10.1, 1.5]), 11.6)
+        self.assertEqual(self.object.sum_numbers([1, -1]), 0)
+
+    def test_sum_returns_None_if_null(self):
+        self.assertEqual(self.object.sum_numbers([]), None)
 
 
 if __name__ == "__main__":
--- week-04/day-03/istvan_kozma_work.py
+++ week-04/day-03/istvan_kozma_work.py
@@ -1,3 +1,11 @@
 class MyObject(object):
     def get_apple(self):
         return "appl"
+
+    def sum_numbers(self, num_list):
+        if num_list:
+            number_sum = 0
+            for num in num_list:
+                number_sum += num
+            return number_sum
+        return
