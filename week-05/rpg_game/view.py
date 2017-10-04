5904d9b400500b32bb68d6760e30f41ab069abc5
Kozma István <pisti_kozma@hotmail.com>
2017-10-04 09:40:57 +0200

    feat(characters): view - add methods to draw characters and get player input

--- week-05/rpg_game/view.py
+++ week-05/rpg_game/view.py
@@ -1,25 +1,45 @@
 import tkinter
 
 class View(object):
-    def __init__(self, game_map):
+    def __init__(self, game_map, hero):
         self.create_canvas()
         self.photo_images = {"floor": tkinter.PhotoImage(file="floor.png"),
-                             "wall": tkinter.PhotoImage(file="wall.png")}
+                             "wall": tkinter.PhotoImage(file="wall.png"),
+                             "hero": tkinter.PhotoImage(file="hero-down.png")}
         self.draw_map(game_map)
-
+        self.player_move_direction = ""
 
     def create_canvas(self):
         self.root = tkinter.Tk()
         self.canvas = tkinter.Canvas(self.root, width=700, height=700)
+        self.root.bind("<KeyPress>", self.player_move_on_keypress)
         self.canvas.pack()
 
     def start(self):
         self.root.mainloop()
 
+    def player_move_on_keypress(self, e):
+        if e.keycode == 37:
+            self.player_move_direction = "left"
+        elif e.keycode == 38:
+            self.player_move_direction = "up"
+        elif e.keycode == 39:
+            self.player_move_direction = "right"
+        elif e.keycode == 40:
+            self.player_move_direction = "down"
+
     def draw_map(self, game_map):
         for y, row in enumerate(game_map):
             for x, tile in enumerate(row):
                 if tile == "_":
-                    self.canvas.create_image(36 + 71 * x, 36 + 71 * y, image=self.photo_images["floor"])
+                    self.canvas.create_image(self.pos_to_pixel(x), self.pos_to_pixel(y), image=self.photo_images["floor"])
                 elif tile == "X":
-                    self.canvas.create_image(36 + 71 * x, 36 + 71 * y, image=self.photo_images["wall"])
+                    self.canvas.create_image(self.pos_to_pixel(x), self.pos_to_pixel(y), image=self.photo_images["wall"])
+
+    def create_entity(self, entity):
+        pos_x = self.pos_to_pixel(entity.pos_x)
+        pos_y = self.pos_to_pixel(entity.pos_y)
+        return self.canvas.create_image(pos_x, pos_y, image=self.photo_images[entity.image])
+
+    def pos_to_pixel(self, position):
+        return 36 + 71 * position
