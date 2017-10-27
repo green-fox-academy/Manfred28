75f2cffec420f823db45e38d31fcd1f859b9c08d
Kozma István <pisti_kozma@hotmail.com>
2017-10-27 12:46:51 +0200

    feat(w07-d04): implement gallery arrow button functionality

--- week-07/day-04/script.js
+++ week-07/day-04/script.js
@@ -1,8 +1,10 @@
 'use strict';
 
-const THUMBNAILS = document.querySelector(".thumbnails")
-let THUMBNAIL_IMAGES = null
-const MAIN_IMAGE = document.querySelector(".main-image-container > img")
+const THUMBNAILS = document.querySelector('.thumbnails');
+let THUMBNAIL_IMAGES = null;
+const MAIN_IMAGE = document.querySelector('.main-image-container > img');
+const GALLERY_ARROW_BUTTON = Array.from(document.querySelectorAll('.main-image-container > button'));
+let currentlyActiveImageIndex = 0;
 const images = [
     {src: 'images/img1.jpg', title: 'Lion'},
     {src: 'images/img2.jpg', title: 'Grass'},
@@ -14,33 +16,56 @@ const images = [
 const createImageNode = function(src) {
     let img = document.createElement('img');
     img.setAttribute('src', src);
-    return img
+    return img;
 }
 
 const addThumbnailImages = function() {
     images.forEach((img) => 
         THUMBNAILS.appendChild(createImageNode(img.src)) 
     )
-    THUMBNAIL_IMAGES = Array.from(THUMBNAILS.children)
+    THUMBNAIL_IMAGES = Array.from(THUMBNAILS.children);
 }
 
 const setMainImage = function(index) {
-    MAIN_IMAGE.setAttribute("src", images[index].src)  
-    MAIN_IMAGE.setAttribute("alt", images[index].title)  
+    MAIN_IMAGE.setAttribute('src', images[index].src)  ;
+    MAIN_IMAGE.setAttribute('alt', images[index].title);
+    THUMBNAIL_IMAGES[currentlyActiveImageIndex].classList.remove('active');
+    THUMBNAIL_IMAGES[index].classList.add('active');
+    currentlyActiveImageIndex = index;
 }
 
-const addClickEventToThumbnailImages = function thumbnailClick () {
+const addClickEventToThumbnailImages = function thumbnailClick() {
     THUMBNAIL_IMAGES.forEach(function(img, index) {
-        img.addEventListener("click", function() {
-            setMainImage(index)
+        img.addEventListener('click', function() {
+            setMainImage(index);
         })
     })
 } 
 
+const AddClickEventToArrowButtons = function galleryButtonClick() {
+    GALLERY_ARROW_BUTTON.forEach(function(button) {
+        button.addEventListener('click', function() {
+            setMainImage( calculateNextActiveImageIndex( parseInt(button.value) ) )
+        })
+    })
+}
+
+const calculateNextActiveImageIndex = function(changedBy) {
+    let newActiveImageIndex = currentlyActiveImageIndex + changedBy
+    if (newActiveImageIndex < 0) {
+        newActiveImageIndex = THUMBNAIL_IMAGES.length - 1;
+    }
+    else if (newActiveImageIndex > THUMBNAIL_IMAGES.length - 1) {
+        newActiveImageIndex = 0;
+    }
+    return newActiveImageIndex
+}
+
 const onLoad = function() {
-    setMainImage(0)
     addThumbnailImages()
     addClickEventToThumbnailImages()
+    setMainImage(0)
+    AddClickEventToArrowButtons()
 }
 
 onLoad()
