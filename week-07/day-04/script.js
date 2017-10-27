2fd6952ffba12414982c2adf463fa4e0b949f1cc
Kozma István <pisti_kozma@hotmail.com>
2017-10-27 11:48:33 +0200

    feat(w07-d03): set main-image to be the first img in the pre-set dict

--- week-07/day-04/script.js
+++ week-07/day-04/script.js
@@ -1,6 +1,7 @@
 'use strict';
 
 const THUMBNAILS = document.querySelector(".thumbnails")
+const MAIN_IMAGE = document.querySelector(".main-image-container > img")
 const images = [
     {src: 'images/img1.jpg', title: 'Lion'},
     {src: 'images/img2.jpg', title: 'Grass'},
@@ -21,7 +22,13 @@ const addThumbnailImages = function() {
     )
 }
 
+const setMainImage = function(index) {
+    MAIN_IMAGE.setAttribute("src", images[index].src)  
+    MAIN_IMAGE.setAttribute("alt", images[index].title)  
+}
+
 const onLoad = function() {
+    setMainImage(0)
     addThumbnailImages()
 }
 
