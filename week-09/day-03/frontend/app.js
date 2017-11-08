6f1ac248f8781b7fab080b7823b4ae1f39add851
Kozma István <pisti_kozma@hotmail.com>
2017-11-08 13:12:36 +0100

    feat(w09-d03): complete greeter exercise

--- week-09/day-03/frontend/app.js
+++ week-09/day-03/frontend/app.js
@@ -22,6 +22,23 @@ app.get('/doubling', function(req, res) {
     }   
 });
 
+app.get('/greeter', function(req, res) {
+    if (req.query.name && req.query.title) {
+        res.json({
+            "welcome_message": `Oh, hi there ${req.query.name}, my dear ${req.query.title}!`,
+        })
+    } else if (!req.query.name) {
+        res.json({
+            "error": "Please provide a name!"
+        })
+    } else if (!req.query.title) {
+        res.json({
+            "error": "Please provide a title!"
+        })
+    }   
+
+});
+
 app.listen(8080);
 
 
