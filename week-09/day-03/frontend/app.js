43a8c2e861fa02437033b75651d00518fe1e7df4
Kozma István <pisti_kozma@hotmail.com>
2017-11-08 12:39:23 +0100

    feat(w09-d03): complete doubling exercise

---
+++ week-09/day-03/frontend/app.js
@@ -0,0 +1,28 @@
+const express = require('express');
+const app = express();
+
+app.use('/assets', express.static('assets'))
+express.json.type = "application/json";
+
+app.get('/', function(req, res){
+  res.sendFile(__dirname + '/index.html');  
+});
+
+app.get('/doubling', function(req, res) {
+    if (req.query.input) {
+        res.json({
+            "received": req.query.input,
+            "result": req.query.input * 2
+        })
+    }
+    else {
+        res.json({
+            "error": "Please provide an input!"
+        })
+    }   
+});
+
+app.listen(8080);
+
+
+
