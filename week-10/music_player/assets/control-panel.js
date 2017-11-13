850d0fd0e9a3fecce7dbce64226585480b6476d8
Kozma István <pisti_kozma@hotmail.com>
2017-11-13 23:15:52 +0100

    feat(music_player) timer is updated as track progresses

--- week-10/music_player/assets/control-panel.js
+++ week-10/music_player/assets/control-panel.js
@@ -1,29 +1,49 @@
 'use strict';
-
 (function() {
+// updateTrackProgress and updateAudioProgress 
+// Might not warrant separate function
 
-const $track = document.querySelector('audio');
+
+const $audioFile = document.querySelector('audio');
 const $playButton = document.querySelector('.track-control button:nth-child(2)');
 const $playButtonImg = $playButton.querySelector('img');
-const $slider = document.querySelector('.track-length-control input[type="range"]')
+const $trackLengthSlider = document.querySelector('.track-length-control input[type="range"]')
+const $currentTime = document.querySelector('.track-length-control .current-time');
+const $trackLength = document.querySelector('.track-length-control .track-length');
 
 
 const togglePlay = function() {
     let sliderUpdateInterval = null;
-    [$playButtonImg.src, $playButtonImg.dataset.altSrc] = [$playButtonImg.dataset.altSrc, $playButtonImg.src];
-    if ($track.paused) {
-        $track.play();
-        sliderUpdateInterval = setInterval(updateSlider, 200)
+    [$playButtonImg.src, $playButtonImg.dataset.altImg] = [$playButtonImg.dataset.altImg, $playButtonImg.src];
+    if ($audioFile.paused) {
+        $audioFile.play();
+        sliderUpdateInterval = setInterval(updateTrackProgress, 200)
     } else {
-        $track.pause();
+        $audioFile.pause();
         clearInterval(sliderUpdateInterval);
     } 
 }
 
-const updateSlider = function() {
-    $slider.value = $track.currentTime / $track.duration * 100
+const updateTrackProgress = function() {
+    $trackLengthSlider.value = $audioFile.currentTime / $audioFile.duration * 100;
+    $currentTime.textContent = convertSecondsToMMSSFormat($audioFile.currentTime);
 }
 
+const convertSecondsToMMSSFormat = function(seconds) {
+    const minutes = Math.floor(seconds / 60);
+    seconds = Math.floor(seconds % 60);
+    seconds = seconds < 10 ? '0' + seconds : seconds;
+    return minutes + ':' + seconds
+}
+
+const updateAudioProgress = function(percentage) {
+    $audioFile.currentTime = $audioFile.duration * percentage;
+}
+
+
 $playButton.addEventListener('click', togglePlay);
+$trackLengthSlider.addEventListener('input', function() {
+    updateAudioProgress(this.value / 100); 
+})
 
 })();
