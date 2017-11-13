b5530f9f90b7af633871d877f8cc8feb027db12b
Kozma István <pisti_kozma@hotmail.com>
2017-11-13 22:33:53 +0100

    feat(music_player) slider moves as track is played

--- week-10/music_player/assets/control-panel.js
+++ week-10/music_player/assets/control-panel.js
@@ -1,14 +1,29 @@
 'use strict';
 
+(function() {
+
 const $track = document.querySelector('audio');
 const $playButton = document.querySelector('.track-control button:nth-child(2)');
 const $playButtonImg = $playButton.querySelector('img');
+const $slider = document.querySelector('.track-length-control input[type="range"]')
 
 
 const togglePlay = function() {
+    let sliderUpdateInterval = null;
     [$playButtonImg.src, $playButtonImg.dataset.altSrc] = [$playButtonImg.dataset.altSrc, $playButtonImg.src];
-    $track.paused ? $track.play() : $track.pause();
+    if ($track.paused) {
+        $track.play();
+        sliderUpdateInterval = setInterval(updateSlider, 200)
+    } else {
+        $track.pause();
+        clearInterval(sliderUpdateInterval);
+    } 
 }
 
-console.log($track.duration)
+const updateSlider = function() {
+    $slider.value = $track.currentTime / $track.duration * 100
+}
+
 $playButton.addEventListener('click', togglePlay);
+
+})();
