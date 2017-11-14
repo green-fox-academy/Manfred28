35897ed5a396c4c62e3445d3ef72a80b1ea698ba
Kozma István <pisti_kozma@hotmail.com>
2017-11-14 16:21:49 +0100

    feat(music_player): add module to update current song info

---
+++ week-10/music_player/assets/current-song.js
@@ -0,0 +1,16 @@
+'use strict';
+
+const currentSong = function() {
+    const $currentSong = document.querySelector('.current-song')
+    const $title = $currentSong.querySelector('.song-info h3');
+    const $band =  $currentSong.querySelector('.song-info span');
+
+    const updateSong = function(track) {
+        $title.textContent = track.title;
+        $band.textContent = track.band
+    }
+
+    return {
+        updateSong
+    }
+}
