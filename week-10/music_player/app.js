86e9c26cb4b34816ccbc732f3c96ce5088b687f9
Kozma István <pisti_kozma@hotmail.com>
2017-11-14 20:23:58 +0100

    fix(): correct mock data

--- week-10/music_player/app.js
+++ week-10/music_player/app.js
@@ -11,8 +11,8 @@ app.get('/', function(req, res) {
 
 app.get('/playlist-tracks', function(req, res) {
     const data = [
-        { "id": 21, "title": "Never Give Up", "artist": "Ars Sonor", "duration": 545, "path": "assets/music/Ars_Sonor_-_02_-_Never_Give_Up.mp3" },
-        { "id": 412, "title": "Doctor Talos Answers The Door", "artist": "Doctor Turtle", "duration": 312.12, "path": "assets/music/Doctor_Turtle_-_Doctor_Talos_Answers_The_Door.mp3" }
+        { "id": 21, "title": "Never Give Up", "artist": "Ars Sonor", "duration": 135, "path": "assets/music/Ars_Sonor_-_02_-_Never_Give_Up.mp3" },
+        { "id": 412, "title": "Doctor Talos Answers The Door", "artist": "Doctor Turtle", "duration": 175, "path": "assets/music/Doctor_Turtle_-_Doctor_Talos_Answers_The_Door.mp3" }
     ]
     res.json(data)
 })
