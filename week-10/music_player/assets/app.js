72e11b278ad9724ec633cd55c5ad5d48e0143de6
Kozma István <pisti_kozma@hotmail.com>
2017-11-14 16:27:18 +0100

    feat(music_player): changing track updates current song info displayed

--- week-10/music_player/assets/app.js
+++ week-10/music_player/assets/app.js
@@ -3,9 +3,21 @@
 (function() {
     const ControlPanel = controlPanel();
     const TrackList = trackList();
+    const CurrentSong = currentSong();
     
-    TrackList.trackOnClick(ControlPanel.loadTrack);
-    ControlPanel.forwardOnClick(() => ControlPanel.loadTrack(TrackList.nextTrack()));
-    ControlPanel.rewindOnClick(() => ControlPanel.loadTrack(TrackList.previousTrack()));
-    ControlPanel.trackOver(() => ControlPanel.loadTrack(TrackList.nextTrack()))
+    TrackList.trackOnClick(track => {
+        ControlPanel.loadTrack(track);
+        CurrentSong.updateSong(track);
+    });
+    ControlPanel.forwardOnClick(() => {
+        TrackList.nextTrack();
+        ControlPanel.loadTrack(TrackList.getCurrentTrack());
+        CurrentSong.updateSong(TrackList.getCurrentTrack());
+    });
+    ControlPanel.rewindOnClick(() => {
+        TrackList.previousTrack();
+        ControlPanel.loadTrack(TrackList.getCurrentTrack());
+        CurrentSong.updateSong(TrackList.getCurrentTrack());
+    });
+    ControlPanel.trackOver(() => ControlPanel.loadTrack(TrackList.nextTrack()));
 })();
