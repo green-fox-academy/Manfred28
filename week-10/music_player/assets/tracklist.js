90735c252d3c3d4bf9a5249465804cbc7b623e71
Kozma István <pisti_kozma@hotmail.com>
2017-11-14 13:59:45 +0100

    feat(music_player): add function to add click events to list items, modularize tracklist.js

--- week-10/music_player/assets/tracklist.js
+++ week-10/music_player/assets/tracklist.js
@@ -1,34 +1,52 @@
 'use strict';
 
-
-const ControlPanel = controlPanel();
-const $tracklist = document.querySelector('.track-list ol');
-console.log($tracklist);
-    
-const tracks = [
+const trackList = function() {
+    const $tracklist = document.querySelector('.track-list ol');
+    let $trackElements = null;
+    console.log($tracklist);
+        
+    const tracks = [
+        {
+        title: 'ayy',
+        url: 'file:///C:/GreenFox/Manfred28/week-10/music_player/assets/music/test.mp3',
+        length: '120'
+    },
     {
-    title: 'ayy',
-    url: 'file:///C:/GreenFox/Manfred28/week-10/music_player/assets/music/test.mp3',
-    length: '120'
-},
-{
-    title: 'party',
-    url: 'file:///C:/GreenFox/Manfred28/week-10/music_player/assets/music/test.mp3',
-    length: '150'
-},
-{
-    title: 'hey',
-    url: 'file:///C:/GreenFox/Manfred28/week-10/music_player/assets/music/test.mp3',
-    length: '100'
-}
-]
+        title: 'party',
+        url: 'file:///C:/GreenFox/Manfred28/week-10/music_player/assets/music/test.mp3',
+        length: '150'
+    },
+    {
+        title: 'hey',
+        url: 'file:///C:/GreenFox/Manfred28/week-10/music_player/assets/music/test.mp3',
+        length: '100'
+    }
+    ]
 
-tracks.forEach(function(elem, i) {
-    const $li = document.createElement('li');
-    $li.innerHTML = `<span class="index">${i+1}</span>
-                    ${elem.title}
-                    <span class="track-length">${elem.length}</span>`
-    $tracklist.appendChild($li);
-})
+    const createTracklistElements = function() {
+        tracks.forEach(function(elem, i) {
+            const $li = document.createElement('li');
+            $li.innerHTML = `<span class="index">${i+1}</span>
+                            ${elem.title}
+                            <span class="track-length">${elem.length}</span>`
+            $tracklist.appendChild($li);
+        })
+        $trackElements = $tracklist.querySelectorAll('li')
+    }
 
-ControlPanel.loadTrack(tracks[0].url)
+    const addTracklistElementEventListener = function (callback) {
+        $trackElements.forEach(function(track) {
+            const index = track.querySelector('.index').textContent
+            track.addEventListener('click', function() {
+                callback(tracks[parseInt(index) - 1]);
+            })
+        })
+    }
+
+    createTracklistElements();
+
+    return {
+        trackOnClick: addTracklistElementEventListener
+    }
+} 
+
