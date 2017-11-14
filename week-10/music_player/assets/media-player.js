cf5e0f93d354a586863c94dec2c0e07a2c837b10
Kozma István <pisti_kozma@hotmail.com>
2017-11-14 18:29:18 +0100

    feat(media_player): get tracks through ajax call

--- week-10/music_player/assets/media-player.js
+++ week-10/music_player/assets/media-player.js
@@ -4,20 +4,36 @@
     const ControlPanel = controlPanel();
     const TrackList = trackList();
     const CurrentSong = currentSong();
+    const AjaxHelper = ajaxHelper();
     
-    TrackList.trackOnClick(track => {
-        ControlPanel.loadTrack(track);
-        CurrentSong.updateSong(track);
-    });
+    const configGetTrack = {
+        method: 'GET',
+        url: 'http://localhost:3000/playlist-tracks'
+    }
+
+    AjaxHelper.ajaxCall(configGetTrack).then((trackList) => {
+        TrackList.getTracks(trackList);
+        TrackList.trackOnClick(track => {
+            ControlPanel.loadTrack(track);
+            CurrentSong.updateSong(track);
+        })
+    })
+
+    
+
     ControlPanel.forwardOnClick(() => {
         TrackList.nextTrack();
         ControlPanel.loadTrack(TrackList.getCurrentTrack());
         CurrentSong.updateSong(TrackList.getCurrentTrack());
     });
+
     ControlPanel.rewindOnClick(() => {
         TrackList.previousTrack();
         ControlPanel.loadTrack(TrackList.getCurrentTrack());
         CurrentSong.updateSong(TrackList.getCurrentTrack());
     });
+
     ControlPanel.trackOver(() => ControlPanel.loadTrack(TrackList.nextTrack()));
+    
+
 })();
