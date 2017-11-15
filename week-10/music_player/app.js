f14fe6946bb70c6a895b21f89e38a9590cae450c
Kozma István <pisti_kozma@hotmail.com>
2017-11-15 11:21:44 +0100

    feat(music_played): get data from database

--- week-10/music_player/app.js
+++ week-10/music_player/app.js
@@ -1,8 +1,9 @@
 'use strict';
 
 const express = require('express');
-const app = express();
+const db = require('./db.js')
 
+const app = express();
 app.use('/assets', express.static('assets'));
 
 app.get('/', function(req, res) {
@@ -10,28 +11,15 @@ app.get('/', function(req, res) {
 });
 
 app.get('/playlist-tracks', function(req, res) {
-    const data = [
-        { "id": 21, "title": "Never Give Up", "artist": "Ars Sonor", "duration": 135, "path": "assets/music/Ars_Sonor_-_02_-_Never_Give_Up.mp3" },
-        { "id": 412, "title": "Doctor Talos Answers The Door", "artist": "Doctor Turtle", "duration": 175, "path": "assets/music/Doctor_Turtle_-_Doctor_Talos_Answers_The_Door.mp3" }
-    ]
-    res.json(data)
+    db.getAllTracks().then(data => res.json(data));
 })
 
-app.get('/playlist-tracks/1', function(req, res) {
-    const data = [
-        { "id": 21, "title": "Never Give Up", "artist": "Ars Sonor", "duration": 135, "path": "assets/music/Ars_Sonor_-_02_-_Never_Give_Up.mp3" },
-    ]
-    res.json(data)
+app.get('/playlist-tracks/:id', function(req, res) {
+    db.getTracksFromPlaylist(req.params.id).then(data => res.json(data));
 })
 
 app.get('/playlists', function(req, res) {
-    const data = [
-        { "id": 1, "title": "Favorites", "system": 1},
-        { "id": 2, "title": "Music for programming", "system": 0},
-        { "id": 3, "title": "Driving", "system": 0},
-        { "id": 5, "title": "Fox house", "system": 0},
-    ]
-    res.json(data)
+    db.getAllPlaylists().then(data => res.json(data));  
 })
 
 
