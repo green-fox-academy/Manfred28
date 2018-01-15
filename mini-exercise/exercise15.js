d5357bd65bfeccc78e6299f4390921d389f764a3
Kozma István <pisti_kozma@hotmail.com>
2018-01-15 10:00:23 +0100

    fix mini-exercise

--- mini-exercise/exercise15.js
+++ mini-exercise/exercise15.js
@@ -1,8 +1,7 @@
 function splitToTwoChars(string) {
   let arrayOfStrings = [];
   for (let i=1; i < string.length; i+= 2) {
-    arrayOfStrings.push([]);
-    arrayOfStrings[arrayOfStrings.length - 1].push(string[i-1] + string[i])
+    arrayOfStrings.push(string[i-1] + string[i])
   }
   return arrayOfStrings
 }
---
+++ mini-exercise/exercise15.rb
@@ -0,0 +1,10 @@
+def splitToTwoChars string
+  array_of_strings = []
+  (1..string.length).step(2) do |i|
+    array_of_strings.push(string[i - 1] + string[i])
+  end
+  array_of_strings
+end
+
+string = "abcdef"
+p splitToTwoChars(string)
